4d64ac17f95d525808999357327e308e29f9a1c7
YeYoJskw <ceratmir@gmail.com>
2026-04-02 18:52:18 +0600

    refactor: restructure components into feature folders

---
+++ src/components/AirConditions/AirConditions.tsx
@@ -0,0 +1,165 @@
+import React, { useMemo } from 'react';
+import WeatherService from '../API/WeatherService';
+import WeatherIcon from '../WeatherIcon';
+import { useQuery } from '@tanstack/react-query';
+import AirConditionsSkeleton from './AirConditionsSkeleton';
+
+import 'react-loading-skeleton/dist/skeleton.css';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+
+interface WeatherItem {
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+  };
+  wind: { speed: number; deg: number; gust: number };
+  pop: number;
+  dt_txt: string;
+  weather: { description: string; icon: string }[];
+}
+
+interface WeatherList {
+  list: WeatherItem[];
+  city: { id: number; name: string };
+  cnt: number;
+  cod: string;
+}
+
+const AirConditions = () => {
+  const todayDate = new Date().toLocaleDateString('en-CA');
+
+  const {
+    data: weather,
+    isLoading,
+    error,
+  } = useQuery<WeatherList>({
+    queryKey: ['weather', 'Moscow'],
+    queryFn: () => WeatherService.getWeather('Moscow'),
+  });
+
+  const groupedForecast = useMemo(() => {
+    if (!weather) return {};
+
+    const grouped: Record<string, WeatherItem[]> = {};
+    const dailySummary: Record<string, WeatherItem> = {};
+
+    for (const forecast of weather.list) {
+      const date = forecast.dt_txt.split(' ')[0];
+      if (!grouped[date]) {
+        grouped[date] = [];
+      }
+      grouped[date].push(forecast);
+    }
+
+    for (const [date, forecasts] of Object.entries(grouped)) {
+      console.log(`Processing date: ${date} with ${forecasts} forecasts`);
+      const middayIndex = Math.floor(forecasts.length / 2);
+      const avgTemp =
+        forecasts.reduce((sum, f) => sum + f.main.temp, 0) / forecasts.length;
+      const avgWindSpeed =
+        forecasts.reduce((sum, f) => sum + f.wind.speed, 0) / forecasts.length;
+      const avgFeelsLike =
+        forecasts.reduce((sum, f) => sum + f.main.feels_like, 0) /
+        forecasts.length;
+
+      dailySummary[date] = {
+        ...forecasts[middayIndex],
+        main: {
+          ...forecasts[middayIndex].main,
+          temp: avgTemp,
+          feels_like: avgFeelsLike,
+        },
+        wind: { ...forecasts[middayIndex].wind, speed: avgWindSpeed },
+      };
+    }
+    console.log('Daily Summary:', dailySummary);
+    return dailySummary;
+  }, [weather]);
+
+  const todayForecast = groupedForecast[todayDate];
+
+  if (error) {
+    return <div>Error loading weather data</div>;
+  }
+
+  if (isLoading) {
+    return <AirConditionsSkeleton />;
+  }
+
+  return (
+    <div className="w-81 h-134.25 bg-[#DEAB4D] rounded-[35px] p-6">
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          <Carousel
+            opts={{
+              align: 'start',
+              loop: true,
+            }}
+            className="w-full carousel-mask"
+          >
+            <CarouselContent>
+              {Object.values(groupedForecast).map((day, idx) => (
+                <CarouselItem key={idx} className="basis-1/5">
+                  <div className="text-center text-white">
+                    <WeatherIcon icon={day.weather[0].icon} />
+                    {new Date(day.dt_txt).toLocaleDateString('en-US', {
+                      weekday: 'short',
+                    })}
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious className="absolute left-0 top-1/2 z-30 -translate-y-1/2" />
+            <CarouselNext className="absolute right-0 top-1/2 z-30 -translate-y-1/2" />
+          </Carousel>
+        </div>
+      )}
+      <div className="pt-16">
+        <div className="font-bold">Air Conditions</div>
+        <div className="flex flex-col gap-8.5 mt-6">
+          <div className="flex  items-center gap-2">
+            <img src="/public/assets/temperature.svg" alt="" />
+            <div className="flex flex-col">
+              <p>Real Feel</p>
+              <p>{todayForecast.main.feels_like.toFixed(1)}°C</p>
+            </div>
+          </div>
+          <div className="flex  items-center gap-2">
+            <img src="/public/assets/wind.svg" alt="" />
+            <div className="flex flex-col">
+              <p>Wind</p>
+              <p>{todayForecast?.wind.speed.toFixed(1)} km/h</p>
+            </div>
+          </div>
+          <div className="flex  items-center gap-2">
+            <img src="/public/assets/water.svg" alt="" />
+            <div className="flex flex-col">
+              <p>Chance of rain</p>
+              <p>{todayForecast?.pop * 100}%</p>
+            </div>
+          </div>
+          <div className="flex  items-center gap-2">
+            <img src="/public/assets/sun.svg" alt="" />
+            <div className="flex flex-col">
+              <p>UV Index</p>
+              <p>0</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AirConditions;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,35 @@
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import WeatherService from '../API/WeatherService';
+
+const Header = () => {
+  const currentDate = new Date();
+  const {
+    data: weather,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['weather', 'Moscow'],
+    queryFn: () => WeatherService.getWeather('Moscow'),
+  });
+
+  console.log(weather);
+
+  return (
+    <div className="pt-6 flex justify-between items-center px-15 mb-6">
+      <div className="info flex flex-col">
+        <div className="flex items-center pb-9">
+          <img className="w-8" src="/assets/marker.svg" alt="point" />
+          <p className="text-2xl">New York</p>
+          <img className="w-5.25 h-7.5" src="/public/assets/Frame.svg" alt="" />
+        </div>
+        <h2 className="text-5xl pb-26">Cloudy</h2>
+        <h1 className="text-[64px] p-0 m-0 leading-none">26°C</h1>
+        <p className="text-lg m-0 p-0">Sunday | 19 Mar 2026</p>
+      </div>
+      <img src="/public/assets/cloud.svg" alt="" />
+    </div>
+  );
+};
+
+export default Header;
